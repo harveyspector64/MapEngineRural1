70877d26ee51a14763d82745dd533e178d8d020f
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-08 23:16:41 -0700

    Update RoadGenerator.js

--- src/features/RoadGenerator.js
+++ src/features/RoadGenerator.js
@@ -5,6 +5,9 @@ import { TILES } from './TerrainGenerator.js';
 
 export default class RoadGenerator {
     constructor(terrain) {
+        if (!terrain) {
+            console.error('Terrain is not defined');
+        }
         this.terrain = terrain;
         this.roads = [];
     }
@@ -66,7 +69,7 @@ export default class RoadGenerator {
         const aStar = new AStar(this.terrain);
         return aStar.findPath(segment[0], segment[segment.length - 1], {
             heuristic: this.heuristic,
-            costFunction: this.cost
+            costFunction: this.cost.bind(this) // Ensure this is bound to the instance
         });
     }
 
@@ -75,7 +78,11 @@ export default class RoadGenerator {
     }
 
     cost(current, neighbor) {
+        if (!this.terrain) {
+            console.error('Terrain is undefined in cost function');
+        }
         const terrainType = this.terrain[neighbor.y][neighbor.x];
+        console.log(`Current: (${current.x}, ${current.y}), Neighbor: (${neighbor.x}, ${neighbor.y}), Terrain: ${terrainType}`);
         switch (terrainType) {
             case TILES.GRASS: return 1;
             case TILES.FIELD: return 2;
