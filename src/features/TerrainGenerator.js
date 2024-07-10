c9ec97d0e5ba93719eb9eb7411678c7bad074281
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-09 22:50:50 -0700

    Update TerrainGenerator.js

--- src/features/TerrainGenerator.js
+++ src/features/TerrainGenerator.js
@@ -6,8 +6,7 @@ export const TILES = {
     FIELD: 'dirt',
     CROP: 'crop',
     TREE: 'tree',
-    BUSH: 'bush',
-    MOUNTAIN: 'mountain'
+    BUSH: 'bush'
 };
 
 const REGION_TYPES = {
@@ -21,14 +20,12 @@ export default class TerrainGenerator {
     constructor(width, height) {
         this.width = width;
         this.height = height;
-        this.noiseSeed = Math.random();
     }
 
     generate() {
         let terrain = this.generateBaseTerrain();
         terrain = this.generateRegionFeatures(terrain);
         terrain = this.smoothTransitions(terrain);
-        terrain = this.addNaturalElements(terrain);
         return terrain;
     }
 
@@ -108,7 +105,6 @@ export default class TerrainGenerator {
     }
 
     smoothTransitions(terrain) {
-        // Simple smoothing: average with neighbors
         const smoothed = JSON.parse(JSON.stringify(terrain));
         for (let y = 1; y < this.height - 1; y++) {
             for (let x = 1; x < this.width - 1; x++) {
@@ -131,16 +127,5 @@ export default class TerrainGenerator {
             return acc;
         }, {});
         return Object.keys(counts).reduce((a, b) => counts[a] > counts[b] ? a : b);
-    }
-
-    addNaturalElements(terrain) {
-        for (let y = 0; y < this.height; y++) {
-            for (let x = 0; x < this.width; x++) {
-                if (terrain[y][x] === TILES.GRASS && Math.random() < 0.1) {
-                    terrain[y][x] = Math.random() < 0.7 ? TILES.BUSH : TILES.TREE;
-                }
-            }
-        }
-        return terrain;
     }
 }
