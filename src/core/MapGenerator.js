b627851884e6a30893e475daddd2a5643966a1e3
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-13 09:26:20 -0700

    Update MapGenerator.js

--- src/core/MapGenerator.js
+++ src/core/MapGenerator.js
@@ -1,4 +1,5 @@
 // src/core/MapGenerator.js
+
 import TerrainGenerator from '../features/TerrainGenerator.js';
 import RoadGenerator from '../features/RoadGenerator.js';
 import StructureGenerator from '../features/StructureGenerator.js';
@@ -6,14 +7,15 @@ import StructureGenerator from '../features/StructureGenerator.js';
 export default class MapGenerator {
     constructor(chunkSize = 64) {
         this.chunkSize = chunkSize;
-        this.terrainGenerator = new TerrainGenerator(chunkSize, chunkSize);
         this.roadGenerator = new RoadGenerator();
         this.structureGenerator = new StructureGenerator();
     }
 
-    generateChunk(chunkX, chunkY) {
+    generateChunk(chunkX, chunkY, seed, regionType) {
+        const terrainGenerator = new TerrainGenerator(this.chunkSize, this.chunkSize, seed, chunkX, chunkY);
+        
         // Generate base terrain for the chunk
-        const terrain = this.terrainGenerator.generate();
+        const terrain = terrainGenerator.generate(regionType);
 
         // Generate structures for the chunk
         const structures = this.structureGenerator.generate(terrain);
@@ -37,14 +39,5 @@ export default class MapGenerator {
             structures,
             roads
         };
-    }
-
-    getNeighboringChunks(chunkX, chunkY) {
-        return [
-            {x: chunkX - 1, y: chunkY},
-            {x: chunkX + 1, y: chunkY},
-            {x: chunkX, y: chunkY - 1},
-            {x: chunkX, y: chunkY + 1}
-        ];
     }
 }
