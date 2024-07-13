912bb457084413bb144cda39fc41c759ab0fcb10
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-13 11:49:39 -0700

    Update UFO.js

--- src/entities/UFO.js
+++ src/entities/UFO.js
@@ -6,11 +6,17 @@ export default class UFO {
         this.y = y;
         this.speed = speed;
         this.sprite = null;
+        this.rotation = 0;
+        
     }
 
     move(dx, dy) {
         this.x += dx * this.speed;
         this.y += dy * this.speed;
+        if (dx !== 0 || dy !== 0) {
+            this.rotation = Math.atan2(dy, dx);
+            }
+        }
     }
 
     getPosition() {
