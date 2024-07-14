c81563f7700078e0e853ce0b333179fd5374fc1a
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-14 12:39:30 -0700

    Update UFO.js

--- src/entities/UFO.js
+++ src/entities/UFO.js
@@ -1,5 +1,7 @@
 // src/entities/UFO.js
 
+import Beam from './Beam.js';
+
 export default class UFO {
     constructor(x, y, speed = 5) {
         this.x = x;
@@ -11,7 +13,7 @@ export default class UFO {
         this.acceleration = 0.99;
         this.maxSpeed = 35;
         this.friction = 0.82;
-        this.beam = new Beam(this);
+        this.beam = new Beam(this); // Add beam to UFO
     }
 
     update() {
@@ -43,12 +45,13 @@ export default class UFO {
         this.x = x;
         this.y = y;
     }
-}
 
+    // New method to toggle beam
     toggleBeam() {
         this.beam.toggle();
     }
 
+    // New method to set beam direction
     setBeamDirection(dx, dy) {
         this.beam.setDirection(dx, dy);
     }
