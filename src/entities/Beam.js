052293f12a48cf4147b82344374729dfce77f311
harveyspector64 <154754949+harveyspector64@users.noreply.github.com>
2024-07-16 19:48:28 -0700

    Update Beam.js

--- src/entities/Beam.js
+++ src/entities/Beam.js
@@ -8,6 +8,9 @@ export default class Beam {
         this.minLength = 64; // Minimum beam length in pixels
         this.maxLength = 320; // Maximum beam length in pixels
         this.length = this.minLength; // Current beam length
+        this.retractionSpeed = 100; // pixels per second
+        this.expansionSpeed = 200; // pixels per second
+        this.targetLength = 0;
     }
 
     activate() {
@@ -25,12 +28,9 @@ export default class Beam {
         }
     }
 
-setLength(length) {
-    this.length = Math.max(0, Math.min(length, this.maxLength));
-    if (this.length === 0) {
-        this.isActive = false;
+    setLength(length) {
+        this.targetLength = Math.max(0, Math.min(length, this.maxLength));
     }
-}
 
     getEndPoint() {
         const ufoPos = this.ufo.getPosition();
@@ -41,10 +41,22 @@ setLength(length) {
     }
 
     increaseLength() {
-        this.length = Math.min(this.length + 5, this.maxLength);
+        this.targetLength = Math.min(this.targetLength + 5, this.maxLength);
     }
 
     decreaseLength() {
-        this.length = Math.max(this.length - 5, this.minLength);
+        this.targetLength = Math.max(this.targetLength - 5, this.minLength);
+    }
+
+    update(deltaTime) {
+        if (this.length < this.targetLength) {
+            this.length = Math.min(this.length + this.expansionSpeed * deltaTime / 1000, this.targetLength);
+        } else if (this.length > this.targetLength) {
+            this.length = Math.max(this.length - this.retractionSpeed * deltaTime / 1000, this.targetLength);
+        }
+
+        if (this.length === 0) {
+            this.isActive = false;
+        }
     }
 }
